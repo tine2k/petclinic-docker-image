3dc38b20b1fa7a88f90ad1d84ee5c4a5ca3f1b3f
Martin Maier-Moessner <martin.maier.moessner@gmail.com>
2017-01-08 20:54:59 +0100

    build with docker-compose

--- docker-openjdk-mysql/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker stop petclinic
-docker rm petclinic
-docker rmi spring-petclinic:1.4.2
-docker build -t spring-petclinic:1.4.2 .
-#docker run --name petclinic -p 80:8080 --network PC spring-petclinic:1.4.2 
